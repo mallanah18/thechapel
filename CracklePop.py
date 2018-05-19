d3dbec021debd285fd7305bef8f0d5f9c3b99fdb
Mustapha Allanah <mallanah@fb.com>
2018-05-19 12:42:40 -0400

    added CracklePop

---
+++ CracklePop.py
@@ -0,0 +1,8 @@
+for num in range(1, 101):
+    if num % 15 == 0:
+        print("CracklePop")
+    elif num % 5 == 0:
+        print("Pop")
+    elif num % 3 == 0:
+        print("Crackle")
+    else: print(num)
